f52fc42da5db6061320ca61a0dcbd746c29e57cc
Sanjum Khan <sanjumk@WHCIT.COM>
2018-04-04 15:07:50 +0530

    First Commit

---
+++ UsingXMLStreamObject/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using XMLStreamToObject;
+
+namespace UsingXMLStreamObject
+{
+    public class Customer
+    {
+        public string CustomerID;
+        public string CompanyName;
+        public string ContactName;
+        public string ContactTitle;
+        public string Address;
+        public string City;
+
+
+    }
+    public  class Program
+    {
+        static void Main(string[] args)
+        {
+            Serializer ser = new Serializer();
+            string path = @"F:\Study Material\Design Pattern Learning\XMLStreamingToObject\UsingXMLStreamObject\XML Data\Customer.xml";
+            string xmlInputData = string.Empty;
+            xmlInputData = File.ReadAllText(path);
+            Customer customer = ser.Deserialize<Customer>(xmlInputData);
+            Console.WriteLine("Cutomer Id: " + customer.CustomerID);
+            Console.WriteLine("CompanyName: " + customer.CompanyName);
+            Console.WriteLine("ContactName: " + customer.ContactName);
+            Console.WriteLine("Address: " + customer.Address);
+            Console.WriteLine("City: " + customer.City);
+            Console.ReadLine();
+        }
+
+      
+    }
+}
---
+++ XMLStreamingToObject/XMLStreamToObject.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.IO;
+using System.Xml.Serialization;
+
+namespace XMLStreamToObject
+{
+    public class Serializer
+    {
+        public  T Deserialize<T>(string input) where T : class
+        {
+            XmlSerializer ser = new XmlSerializer(typeof(T));
+
+            using (StringReader sr = new StringReader(input))
+            {
+                return (T)ser.Deserialize(sr);
+            }
+        }        
+
+    }
+
+}
